6896019ae5fa60c69ced2ddbe0e76a05fef3bf47
Irina <75871942+irina-polybius@users.noreply.github.com>
2025-04-19 15:30:28 +0100

    updated models for each table

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,37 @@
 from pydantic import BaseModel, EmailStr, constr
+from typing import Optional
+from datetime import datetime
+import uuid
 
+# users model
 class UserRegistration(BaseModel):
-    username: constr(min_length=3, max_length=50)
     full_name: constr(min_length=2, max_length=100)
     email: EmailStr
-    company_name: constr(min_length=2, max_length=100)
+    company_id: UUID
     password: constr(min_length=8)
 
 class UserResponse(BaseModel):
-    id: str
-    username: str
+    id: uuid.UUID
     full_name: str
-    email: str
-    company_name: str 
-    
+    email: EmailStr
+    company_id: uuid.UUID
+    created_at: datetime
+    updated_at: datetime
+
+# companies model
+class CompanyRegistration(BaseModel):
+    name: str
+    email: EmailStr
+    registered_address: Optional[str] = None
+    password: str 
+
+class CompanyResponse(BaseModel):
+    id: uuid.UUID
+    created_at: datetime
+    updated_at: datetime
+
+# login_log model
+class LoginLogResponse(BaseModel):
+    id: int
+    user_id: uuid.UUID
+    login_time: datetime
