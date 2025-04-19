b8ccdd1f9130343b7f58dbe6f3dc0522c06d38e3
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2025-04-19 15:43:39 +0100

    adding new changes to the backend

--- backend/models.py
+++ backend/models.py
@@ -1,36 +1,44 @@
-from pydantic import BaseModel, EmailStr, constr
-from typing import Optional
+from pydantic import BaseModel, EmailStr
+from pydantic.types import constr
+from typing import Annotated, Optional
 from datetime import datetime
 import uuid
 
-# users model
-class UserRegistration(BaseModel):
-    full_name: constr(min_length=2, max_length=100)
+# Company Models
+class CompanyRegistration(BaseModel):
+    name: Annotated[str, constr(min_length=2, max_length=255)]
     email: EmailStr
-    company_id: UUID
-    password: constr(min_length=8)
+    password: Annotated[str, constr(min_length=8)]
+    registered_address: Optional[str] = None
+
+class CompanyResponse(BaseModel):
+    id: uuid.UUID
+    name: str
+    email: EmailStr
+    registered_address: Optional[str]
+    created_at: datetime
+    updated_at: datetime
+
+# User Models
+class UserRegistration(BaseModel):
+    full_name: Annotated[str, constr(min_length=2, max_length=255)]
+    email: EmailStr
+    company_id: uuid.UUID
+    password_hash: Annotated[str, constr(min_length=8)]
 
 class UserResponse(BaseModel):
     id: uuid.UUID
     full_name: str
     email: EmailStr
     company_id: uuid.UUID
+    password_hash: str
     created_at: datetime
     updated_at: datetime
 
-# companies model
-class CompanyRegistration(BaseModel):
-    name: str
-    email: EmailStr
-    registered_address: Optional[str] = None
-    password: str 
+# Login Log Models
+class LoginLogRegistration(BaseModel):
+    user_id: uuid.UUID
 
-class CompanyResponse(BaseModel):
-    id: uuid.UUID
-    created_at: datetime
-    updated_at: datetime
-
-# login_log model
 class LoginLogResponse(BaseModel):
     id: int
     user_id: uuid.UUID
